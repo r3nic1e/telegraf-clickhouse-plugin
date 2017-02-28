a63682be71ea603d6193ced0b6d83cfaa8598d76
Svyatoslav Dumchenko <sdumchenko@beget.ru>
2017-02-28 17:28:06 +0300

    Remove usage of internal package, actually add metrics

--- clickhouse_client.go
+++ clickhouse_client.go
@@ -1,11 +1,10 @@
 package telegraf_beget_clickhouse_plugin
 
 import (
+	"fmt"
 	"time"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/plugins/outputs"
 	"github.com/roistat/go-clickhouse"
 )
@@ -15,13 +14,13 @@ type ClickhouseClient struct {
 	Username   string
 	Password   string
 	Database   string
-	Timeout    internal.Duration
+	Timeout    time.Duration
 	connection *clickhouse.Conn
 }
 
 func (c *ClickhouseClient) Connect() error {
 	transport := clickhouse.NewHttpTransport()
-	transport.Timeout = c.Timeout.Duration
+	transport.Timeout = c.Timeout
 
 	c.connection = clickhouse.NewConn(c.URL, transport)
 
@@ -31,28 +30,71 @@ func (c *ClickhouseClient) Connect() error {
 	}
 	return nil
 }
+
 func (c *ClickhouseClient) Close() error {
 	return nil
 }
+
 func (c *ClickhouseClient) Description() string {
 	return "Configuration for clickhouse server to send metrics to"
 }
+
 func (c *ClickhouseClient) SampleConfig() string {
-	return ""
+	return `
+# URL to connect
+url = "http://localhost:8123"
+# Username and password to auth
+username = "default"
+password = ""
+# Database to insert data
+database = "default"`
 }
+
 func (c *ClickhouseClient) Write(metrics []telegraf.Metric) error {
-	spew.Dump(metrics)
+	for _, metric := range metrics {
+		table := metric.Name()
+
+		var columns clickhouse.Columns
+		var row clickhouse.Row
+
+		for name, value := range metric.Tags() {
+			columns = append(columns, name)
+			row = append(row, value)
+		}
+
+		for name, value := range metric.Fields() {
+			columns = append(columns, name)
+			row = append(row, value)
+		}
+
+		columns = append(columns, "date", "datetime")
+
+		date := metric.Time().Format("2006-01-02")
+		datetime := metric.Time().Format("2006-01-02 15:04:05")
+		row = append(row, date, datetime)
+
+		query, err := clickhouse.BuildInsert(table, columns, row)
+		fmt.Println(query.Stmt)
+		if err != nil {
+			return err
+		}
+
+		err = query.Exec(c.connection)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 func newClickhouse() *ClickhouseClient {
 	return &ClickhouseClient{
-		Timeout: internal.Duration{Duration: time.Minute},
+		Timeout: time.Minute,
 	}
 }
 
 func init() {
-	outputs.Add("clickhouse_client", func() telegraf.Output {
+	outputs.Add("clickhouse", func() telegraf.Output {
 		return newClickhouse()
 	})
 }
