ab7828db22de13aa47b72429a94123341341e008
Svyatoslav Dumchenko <sdumchenko@beget.ru>
2017-03-02 22:36:28 +0300

    Superdirty fix for dirty fix for shitty influx

--- clickhouse_client.go
+++ clickhouse_client.go
@@ -35,8 +35,9 @@ func newClickhouseMetric(metric telegraf.Metric) *clickhouseMetric {
 		cm.AddData(name, value, true)
 	}
 
-	date := metric.Time().Format("2006-01-02")
-	datetime := metric.Time().Format("2006-01-02 15:04:05")
+	metricTime := metric.Time().Truncate(6 * time.Hour)
+	date := metricTime.Format("2006-01-02")
+	datetime := metricTime.Format("2006-01-02 15:04:05")
 	cm.AddData("date", date, true)
 	cm.AddData("datetime", datetime, true)
 
